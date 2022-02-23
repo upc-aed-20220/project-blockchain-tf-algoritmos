bdea4d8470b4faa87e0eb2729663284d669839af
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-02-23 11:18:02 -0500

    Versión Final

--- Usuario.h
+++ Usuario.h
@@ -5,8 +5,16 @@ using namespace std;
 struct Usuario
 {
     string nombre;
-    DoubleList<Transaccion>* transaccionesEmitidas;
-    DoubleList<Transaccion>* transaccionesRecibidas;
+    DoubleList<Transaccion> *transaccionesEmitidas;
+    DoubleList<Transaccion> *transaccionesRecibidas;
+
+    Usuario(string nombre, bool empty)
+    {
+        this->nombre = nombre;
+        transaccionesEmitidas = new DoubleList<Transaccion>();
+        transaccionesRecibidas = new DoubleList<Transaccion>();
+    }
+
     Usuario(string _nombre = "")
     {
         transaccionesEmitidas = new DoubleList<Transaccion>();
@@ -15,32 +23,39 @@ struct Usuario
     }
 
     string getNombre() { return this->nombre; }
-
-    void ingresarTransaccionEmitida(Transaccion trans){
-        transaccionesEmitidas->push_back(trans);
+    void ingresarTransaccionEmitida(Transaccion trans) { transaccionesEmitidas->push_back(trans); }
+    void ingresarTransaccionRecibida(Transaccion trans) { transaccionesRecibidas->push_back(trans); }
+    void mostrarTransaccionesEmitidas()
+    {
+        for (auto elem : *transaccionesEmitidas)
+        {
+            cout << elem.getString() << "\n";
+        }
     }
-    void ingresarTransaccionRecibida(Transaccion trans){
-        transaccionesRecibidas->push_back(trans);
-    }
-
 };
 
-ostream& operator<<(ostream& os, Usuario usuario) {
+ostream &operator<<(ostream &os, Usuario usuario)
+{
     string Emitidos = "";
     string Recibidos = "";
 
     DoubleList<Transaccion>::Iterator it = usuario.transaccionesEmitidas->begin();
-    for (it; it != usuario.transaccionesEmitidas->end(); ++it) {
+    for (it; it != usuario.transaccionesEmitidas->end(); ++it)
+    {
         Emitidos.append((*it).getString());
         Emitidos.append("\n");
     }
 
     DoubleList<Transaccion>::Iterator it2 = usuario.transaccionesRecibidas->begin();
-    for (it2; it2 != usuario.transaccionesRecibidas->end(); ++it2) {
+    for (it2; it2 != usuario.transaccionesRecibidas->end(); ++it2)
+    {
         Recibidos.append((*it2).getString());
         Recibidos.append("\n");
     }
-    
-    os << "Usuario: " << usuario.nombre << "\n" << "Transacciones Emitidas:\n" << Emitidos<<"\nTransacciones Recibidas\n" << Recibidos;
+
+    os << "Usuario: " << usuario.nombre << "\n"
+       << "Transacciones Emitidas:\n"
+       << Emitidos << "\nTransacciones Recibidas\n"
+       << Recibidos<<"\n_________________________\n";
     return os;
 }
