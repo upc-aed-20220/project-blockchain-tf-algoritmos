0107b42c5d4e67ca255e6bb22a3842a3d7b471a9
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-02-23 11:16:44 -0500

    Versión Final

--- Bloque.h
+++ Bloque.h
@@ -1,62 +1,89 @@
 #include "double.h"
 #include "Usuario.h"
-
-struct Bloque
+#include "nodeArbol.h"
+#include <time.h>
+class Bloque
 {
-    Node<Bloque>* node;
+
+private:
+    NodeBT<Bloque *> *node;
     string key;
     string prev_key;
-    int index;
-    DoubleList<Transaccion>* transacciones;
+    size_t fechaInsercion;
+    DoubleList<Transaccion> *transacciones;
     int limiteTransacciones;
 
-    Bloque() {
-        // key = hashFunction(key);
+public:
+    Bloque()
+    {
         prev_key = "00000000000";
         limiteTransacciones = 5;
         transacciones = nullptr;
+        fechaInsercion = time(0);
         node = nullptr;
         transacciones = new DoubleList<Transaccion>();
     }
 
-    int getNumeroTransacciones() { return this->transacciones->size(); }
-    string returnKey() { return this->key; }
-     
-    void insertarTransaccion(Transaccion transaccion) {
-        //if (getNumeroTransacciones() >= this->limiteTransacciones)
-        //    return;
-         this->transacciones->push_back(transaccion);
-        // TODO: Debo de insertar un puntero al arreglo o lista de transacciones del usuario que realizo la transaccion
-        // TODO: se debe actualizar la key cada vez que se inserte una transaccion
-        return;
+    void setNode(NodeBT<Bloque *> *b) { node = b; }
+    NodeBT<Bloque *> *getNode() { return this->node; }
+
+    bool operator<=(Bloque b)
+    {
+        return this->getFechaInsercion() <= b.getFechaInsercion();
+    }
+    bool operator>(Bloque b)
+    {
+        return this->getFechaInsercion() > b.getFechaInsercion();
+    }
+    bool operator==(Bloque b)
+    {
+        return this->getFechaInsercion() == b.getFechaInsercion();
+    }
+    bool operator<(Bloque b)
+    {
+        return this->getFechaInsercion() < b.getFechaInsercion();
     }
 
-    string mostrarTransacciones() {
-        if (this->transacciones == nullptr) {
-            return "Esto es nulo :)";
+    DoubleList<Transaccion> *getTransacciones() { return this->transacciones; }
+    size_t getFechaInsercion() { return this->fechaInsercion; }
+    void setKey(string key) { this->key = key; }
+    void setPrevKey(string _prevkey = "00000000000") { this->prev_key = _prevkey; }
+    int getNumeroTransacciones() { return this->transacciones->size(); }
+    string getKey() { return this->key; }
+    string getPrevKey() { return this->prev_key; }
+
+    void insertarTransaccion(Transaccion transaccion)
+    {
+        this->transacciones->push_back(transaccion);
+    }
+
+    string mostrarTransacciones()
+    {
+        if (this->transacciones == nullptr)
+        {
+            return "No hay bloques con transacciones";
         }
         string output = "";
         DoubleList<Transaccion>::Iterator it = transacciones->begin();
-        for (it; it != transacciones->end(); ++it) {
+        for (it; it != transacciones->end(); ++it)
+        {
             output.append((*it).getString());
-            output.append("\n");
         }
-        /*transacciones;
-        for (auto it : *transacciones) {
-            output = output + it.getString() + "\n";
-        }*/
+
+        output.append("\n");
         return output;
     }
 
-    string generarKey() {
+    string generarKey()
+    {
         string temp_string;
         temp_string = "000";
-       
+
         temp_string = temp_string + (char)transacciones->back().getEmisor()[0];
-        temp_string = temp_string + to_string((transacciones->back().getEmisor()[0] * 1)% 100) ;
+        temp_string = temp_string + to_string((transacciones->back().getEmisor()[0] * 1) % 100);
 
         temp_string = temp_string + (char)transacciones->back().getReceptor()[0];
-        temp_string = temp_string + to_string((transacciones->back().getReceptor()[0] * 1)% 100) ;
+        temp_string = temp_string + to_string((transacciones->back().getReceptor()[0] * 1) % 100);
 
         float temp_monto = transacciones->back().getMonto();
         string string_monto = to_string(temp_monto);
@@ -66,31 +93,16 @@ struct Bloque
         nuevaKey.resize(11);
 
         int num;
-        for (int i = 0; i < 11; i++) {
-            //num = (temp_string[i] + prev_key[i])/2;
+        for (int i = 0; i < 11; i++)
+        {
             num = temp_string[i] + prev_key[i];
             if (i % 2 == 0)
                 num = 48 + num % 10;
             else
                 num = 65 + num % 25;
             nuevaKey[i] = char(num);
-
-           
         }
 
         return nuevaKey;
-        //this->key = temp_string;;
     }
-
-    void setPrevKey(string _prevkey = "00000000000") {
-        this->prev_key = _prevkey;
-    }
-
-    void setIndex(int _index) {
-        this->index = _index;
-    }
-
-    int getIndex() { return this->index + 1; }
-
-    string getKey() { return this->key; }
 };
