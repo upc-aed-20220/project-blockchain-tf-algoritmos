069f606acbdf3d4685d183d3c29def300468ac8e
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-02-23 11:16:25 -0500

    Versión 2.0

--- BlockChain.h
+++ BlockChain.h
@@ -1,185 +1,267 @@
 #include "HashTable.h"
-class BlockChain {
-    private:
-        DoubleList<Usuario>* usuarios;
-    //BSTree<Bloque<Transaccion>*>* porNombre;
-    //BSTree<Bloque<Transaccion>*>* porNonce; // rango inicial : 20-100 , puede salir repetidos, cada 5 o 10 repetidos aumentar el rango de 20 a 120
-    //BSTree<bloque<Transaccion>*>* por;
-        HashTable* hashTable;
+#include "Arbol.h"
+class BlockChain
+{
+private:
+    Arbol<Usuario *> *usuarios;
+    Arbol<Bloque *> *arbolFecha;
+    Arbol<Transaccion> *arbolTransacciones;
+    Arbol<Transaccion> *arbolTransaccionesMonto;
+    HashTable *hashTable;
 
-    public:
-    BlockChain() {
-        hashTable = new HashTable();
-        usuarios = new DoubleList<Usuario>();
-    }
-
-    string setBlockinHashTable(Bloque bloque) {
-        return hashTable->set(bloque);
-    }
-
-    Bloque recalcularHash(Bloque bloque) {
-        return hashTable->recalcularHash(bloque);
-    }
-
-    int getHashSize() {
-        return hashTable->getSize();
-    }
-
-    bool verificarExistenciaBloque(string key) {
-        return hashTable->keyExists(key);
-    }
-
-    void mostrarTodasLasTransacciones() {
-        hashTable->mostrarTransacciones();
-    }
-
-    void mostrarTodosLosUsuarios() {
-        int i = 1;
-        for (auto usuario : (*usuarios)) {
-            cout << "\tUsuario: " << i << ":\t" << usuario.getNombre() << endl;
-            i++;
-        }
-        cout << endl;
-    }
-
-    Bloque retornarBloque(string key)
+public:
+    BlockChain()
     {
-        return hashTable->retornarBloque(key);
+        hashTable = new HashTable();
+        auto showU = [](Usuario *user) -> void
+        { cout << *user << "\n"; };
+        auto compU = [](Usuario *a, Usuario *b) -> bool
+        { return a->getNombre() < b->getNombre(); };
+        auto equalsU = [](Usuario *a, Usuario *b) -> bool
+        { return a->getNombre() == b->getNombre(); };
+        auto dfU = []() -> Usuario *
+        { return nullptr; };
+        usuarios = new Arbol<Usuario *>(compU, showU, equalsU, dfU);
+
+        auto showB = [](Bloque *b) -> void
+        { cout << b->mostrarTransacciones() << " "; };
+        auto compB = [](Bloque *a, Bloque *b) -> bool
+        { return *a < *b; };
+        auto equalsB = [](Bloque *a, Bloque *b) -> bool
+        { return *a == *b; };
+        auto dfB = []() -> Bloque *
+        { return nullptr; };
+        arbolFecha = new Arbol<Bloque *>(compB, showB, equalsB, dfB);
+
+        auto showT = [](Transaccion b) -> void
+        { cout << b.show() << "\n"; };
+        auto compT = [](Transaccion a, Transaccion b) -> bool
+        { return a < b; };
+        auto equalsT = [](Transaccion a, Transaccion b) -> bool
+        { return a == b; };
+        auto dfT = []() -> Transaccion
+        { return Transaccion(); };
+        arbolTransacciones = new Arbol<Transaccion>(compT, showT, equalsT, dfT);
+
+        auto showTM = [](Transaccion b) -> void
+        { cout << b.show() << "\n"; };
+        auto compTM = [](Transaccion a, Transaccion b) -> bool
+        { return a.getMonto() < b.getMonto(); };
+        auto equalsTM = [](Transaccion a, Transaccion b) -> bool
+        { return a.getMonto() == b.getMonto(); };
+        auto dfTM = []() -> Transaccion
+        { return Transaccion(); };
+        arbolTransaccionesMonto = new Arbol<Transaccion>(compTM, showTM, equalsTM, dfTM);
     }
 
-    string ingresarUsuario(Usuario user){
+    HashTable *getHashTable() { return this->hashTable; } 
+
+    int getHashSize() { return hashTable->getSize(); }
+
+    void recalcularKeyDesdeBloque(Bloque *bloque)
+    {
+        NodeBT<Bloque *> *parent = nullptr;
+        NodeBT<Bloque *> *node = bloque->getNode();
+        arbolFecha->propagar(node, parent, node->data->getKey(), hashTable);
+    }
+
+    void buscarPorFechaArbolTransaccion(int dia, int mes, int anio)
+    {
+        Transaccion t = Transaccion(dia, mes, anio);
+        arbolTransacciones->find(t);
+    }
+    void buscarPorMontoArbolTransaccionMonto(int begin, int end)
+    {
+        auto isInRange = [begin, end](Transaccion t) -> int
+        {
+            return (t.getMonto() > begin && t.getMonto() < end) ? 0 : (t.getMonto() == end) ? 1
+                                                                  : (t.getMonto() == begin) ? 2
+                                                                  : (t.getMonto() < begin)  ? 3
+                                                                  : (t.getMonto() > end)    ? 4
+                                                                                            : -1;
+        };
+        arbolTransaccionesMonto->findyRange(isInRange);
+    }
+    void buscarTransaccionesEmitidasDeUnEmisorX(string nombre)
+    {
+        Usuario *user = buscaUsuario(nombre);
+        if (!user)
+        {
+            cout << "\nEmisor No Encontrado\n";
+            return;
+        }
+        user->mostrarTransaccionesEmitidas();
+        user = nullptr;
+    }
+
+    void insertarBloqueEnArbol(Bloque *bloque)
+    {
+        NodeBT<Bloque *> *parent = nullptr;
+        NodeBT<Bloque *> *node = arbolFecha->insert2(bloque, parent);
+
+        bloque->setNode(node);
+        parent ? bloque->setPrevKey(parent->data->getKey()) : bloque->setPrevKey("00000000000");
+    }
+
+    void insertarTransaccionEnArbol(Transaccion transaccion)
+    {
+        NodeBT<Transaccion> *p = nullptr;
+        arbolTransacciones->insert(transaccion, p);
+        arbolTransaccionesMonto->insert(transaccion, p);
+        p = nullptr;
+    }
+
+    void ingresarTransaccionUsuario(Transaccion trans)
+    {
+        insertarTransaccionEnArbol(trans);
+        Usuario *aux = buscaUsuario(trans.getEmisor());
+        aux->ingresarTransaccionEmitida(trans);
+        Usuario *aux2 = buscaUsuario(trans.getReceptor());
+        aux2->ingresarTransaccionRecibida(trans);
+    }
+
+    string setBlockinHashTable(Bloque *bloque)
+    {
+        insertarBloqueEnArbol(bloque);
+        string key = hashTable->setBlockinHashTable(bloque);
+        return key;
+    }
+
+    void ingresarUsuario(Usuario *user)
+    {
         string temp_string = "";
-        for (auto usuario : (*usuarios)) {
-            if (user.getNombre() == usuario.getNombre()) {
-                temp_string = "\nEl usuario ya existe";
-                return temp_string;
-            }
+        if (usuarios->findValue(user))
+        {
+            cout << "\nUsuario Existente\n";
         }
-        usuarios->push_back(user);
-        temp_string = "\nUsuario ingresado correctamente";
-        return temp_string;
-    }
-
-    //bool registroUsuario(string datos, string alias, string contra, int dinero) {
-    //    Usuario* nuevoU = buscarUsuario(usuarios, alias);
-    //    if (nuevoU != nullptr) {
-    //        cout << "\nUsuario ya registrado, ingrese un nuevo alias ";
-    //        return false;
-    //    }
-    //    nuevoU = new Usuario(datos, alias, contra, dinero);
-    //    usuarios.push_back(nuevoU);
-    //    return true;
-    //}
-
-    bool revisarUsuarioExistente(string nombre) {
-        for (auto usuario : (*usuarios)){
-            if (nombre == usuario.getNombre()) {
-                return true;
-            }
+        else
+        {
+            NodeBT<Usuario *> *p = nullptr;
+            usuarios->insert(user, p);
+            p = nullptr;
+            cout << "\n Usuario ingresado correctamente\n";
         }
-        return false;
     }
 
-    void ingresarTransaccionUsuario(Transaccion trans) {
-        buscaUsuario(trans.getEmisor()).ingresarTransaccionEmitida(trans);
-        buscaUsuario(trans.getReceptor()).ingresarTransaccionRecibida(trans);
+    Bloque *recalcularHash(Bloque *bloque) { return hashTable->recalcularHash(bloque); }
+
+    Bloque *retornarBloque(string key) { return hashTable->retornarBloque(key); }
+
+    Usuario *buscaUsuario(string nombre)
+    {
+        Usuario aux = Usuario(nombre, true);
+        return usuarios->findValue(&aux);
     }
 
-    Usuario buscaUsuario(string nombre) {
-        for (auto usuario : (*usuarios))
-            if (nombre == usuario.getNombre())
-                return usuario;
+    bool verificarExistenciaBloque(string key) { return hashTable->keyExists(key); }
+
+    bool revisarUsuarioExistente(string nombre)
+    {
+        Usuario aux(nombre, true);
+        return usuarios->findValue(&aux) != nullptr;
     }
 
-    void filtradoUsuarios(int opc) {
+    void mostrarTodasLasTransacciones() { hashTable->mostrarTransacciones(); }
+
+    void mostrarTodosLosUsuarios()
+    {
+        usuarios->displayPreOrder();
+    }
+
+    void filtradoUsuarios(int opc)
+    {
         string nombre;
         char character;
-        bool seEncontro = false;
-            switch (opc) {
-            case 1:
-                cout << "\nIngrese nombre a buscar:";
-                cin >> nombre;
-                cout << "\n";
-                for (auto usuario : (*usuarios)) {
-                    primeraPalabra(nombre, usuario,&seEncontro);
-                }break;
-            case 2:
-                cout << "\nIngrese primera letra:";
-                cin >> character;
-                cout << "\n";
-                for (auto usuario : (*usuarios)) {
-                    iniciaPalabra(character, usuario,&seEncontro);
-                }
-                break;
-            case 3:
-                cout << "\nIngrese ultima letra:";
-                cin >> character;
-                cout << "\n";
-                for (auto usuario : (*usuarios)) {
-                    ultimaPalabra(character, usuario,&seEncontro);
-                }
-                break;
-            case 4:
-                cout << "\nIngrese letra/s contenidas en:";
-                cin >> nombre;
-                cout << "\n";
-                for (auto usuario : (*usuarios)) {
-                    mediasPalabras(nombre, usuario,&seEncontro);
-                }
-                break;
-            case 5:
-                cout << "\nIngrese letra/s no contenidas en:";
-                cin >> nombre;
-                cout << "\n";
-                for (auto usuario : (*usuarios)) {
-                    noMediasPalabras(nombre, usuario,&seEncontro);
-                }
-                break;
-                default:
-                break;
-            }
-            if (!seEncontro) {
-                cout <<  "\n\nNO EXISTEN RESULTADOS PARA EL FILTRO DADO";
-            }
-            cout << endl; system("pause");
+        switch (opc)
+        {
+        case 1:
+            cout << "\nIngrese nombre a buscar:";
+            cin >> nombre;
+            cout << "\n";
+            usuarios->findByCriteria([&, this](Usuario *user) -> bool
+                                     { return primeraPalabra(nombre, user); });
+            break;
+        case 2:
+            cout << "\nIngrese primera letra:";
+            cin >> character;
+            cout << "\n";
+            usuarios->findByCriteria([&, this](Usuario *user) -> bool
+                                     { return iniciaPalabra(character, user); });
+            break;
+        case 3:
+            cout << "\nIngrese ultima letra:";
+            cin >> character;
+            cout << "\n";
+            usuarios->findByCriteria([&, this](Usuario *user) -> bool
+                                     { return ultimaPalabra(character, user); });
+            break;
+        case 4:
+            cout << "\nIngrese letra/s contenidas en:";
+            cin >> nombre;
+            cout << "\n";
+            usuarios->findByCriteria([&, this](Usuario *user) -> bool
+                                     { return mediasPalabras(nombre, user); });
+            break;
+        case 5:
+            cout << "\nIngrese letra/s no contenidas en:";
+            cin >> nombre;
+            cout << "\n";
+            usuarios->findByCriteria([&, this](Usuario *user) -> bool
+                                     { return noMediasPalabras(nombre, user); });
+            break;
         }
-    
-
-    void primeraPalabra(string palabra, Usuario usuario,bool *seEncontro) {
-
-        string nombre = usuario.getNombre();
-        if (nombre == palabra) {
-            cout << usuario << endl;
-            *seEncontro = true;
-        }
-    }
-    void iniciaPalabra(char letraInicial, Usuario usuario, bool* seEncontro) {
-        string nombre = usuario.getNombre();
-        if (toupper(nombre[0]) == toupper(letraInicial)) {
-            cout << usuario << endl;
-            *seEncontro = true;
-        }
-    }
-    void ultimaPalabra(char letraFinal, Usuario usuario, bool* seEncontro) {
-        string nombre = usuario.getNombre();
-        if (nombre[nombre.length() - 1] == letraFinal) {
-            cout << usuario << endl;
-            *seEncontro = true;
-        }
-    }
-    void mediasPalabras(string letrasMedias, Usuario usuario, bool* seEncontro) {
-        string nombre = usuario.getNombre();
-        if (nombre.rfind(letrasMedias) != string::npos) {
-            cout << usuario<<endl;
-            *seEncontro = true;
-        }
-    }
-    void noMediasPalabras(string noLetrasMedias, Usuario usuario, bool* seEncontro) {
-        string nombre = usuario.getNombre();
-        if (nombre.rfind(noLetrasMedias) == string::npos) {
-            cout << usuario << endl;
-            *seEncontro = true;
-        }
+        cout << "\n";
+        system("pause");
     }
 
+    bool primeraPalabra(string palabra, Usuario *usuario)
+    {
+        string nombre = usuario->getNombre();
+        if (nombre == palabra)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    bool iniciaPalabra(char letraInicial, Usuario *usuario)
+    {
+        string nombre = usuario->getNombre();
+        if (toupper(nombre[0]) == toupper(letraInicial))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    bool ultimaPalabra(char letraFinal, Usuario *usuario)
+    {
+        string nombre = usuario->getNombre();
+        if (nombre[nombre.length() - 1] == letraFinal)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    bool mediasPalabras(string letrasMedias, Usuario *usuario)
+    {
+        string nombre = usuario->getNombre();
+        if (nombre.rfind(letrasMedias) != string::npos)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    bool noMediasPalabras(string noLetrasMedias, Usuario *usuario)
+    {
+        string nombre = usuario->getNombre();
+        if (nombre.rfind(noLetrasMedias) == string::npos)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
 };
